8d179255bef7d94b9a2a36ef27d43158132dc770
AsumVictor <iamasum369@gmail.com>
2023-08-02 09:57:22 +0000

    Initialize tailwind

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Missions from './components/Missions';
-import MyProfile from './components/MyProfile';
-import Rockets from './components/Rockets';
 import Nav from './components/Nav';
 
 const App = () => (
   <div className="App">
     <Nav />
-
-    <Routes>
-      <Route path="/" element={<Rockets />} />
-      <Route path="/Missions" element={<Missions />} />
-      <Route path="/MyProfile" element={<MyProfile />} />
-    </Routes>
   </div>
 );
 
